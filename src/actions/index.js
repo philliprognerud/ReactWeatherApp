d300d522801d80fee3725150d71259a4ac711847
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-07-26 05:48:07 +0000

    added components to weather app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = 'ffaed06854f4d2b255a878995b987eb5';
+import axios from 'axios';
 
+const API_KEY = 'ffaed06854f4d2b255a878995b987eb5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
